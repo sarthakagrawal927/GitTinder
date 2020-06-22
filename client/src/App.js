aff9231dc892786782c73ad18e34b5e6e345d448
Sarthak Agrawal <43884471+sarthakagrawal927@users.noreply.github.com>
2020-06-22 17:56:36 +0530

    seo attempt

--- client/src/App.js
+++ client/src/App.js
@@ -20,52 +20,57 @@ const App = () => {
   }, []);
 
   return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Helmet
-            title='GitTinder'
-            meta={[
-              {
-                name: "description",
-                content:
-                  "GitTinder is a social networking and online developer meeting application/website that allows users to connect with similar minds",
-              },
-              {
-                property: "og:description",
-                content:
-                  "GitTinder is a social networking and online developer meeting application/website that allows users to connect with similar minds",
-              },
-              {
-                property: "og:title",
-                content: "GitTinder GitHub Tinder DevConnector",
-              },
-              {
-                property: "og:image",
-                content: "http://example.com/article.jpg",
-              },
-              { property: "og:url", content: "http://gittinder.herokuapp.com" },
-              { property: "og:type", content: "social-media" },
-              {
-                name: "keywords",
-                content:
-                  "GitHub Tinder GitTinder social-media hub developers connect",
-              },
-              {
-                name: "author",
-                content: "Sarthak Agrawal Significant Hobbies GitTinder",
-              },
-            ]}
-          />
-
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={Landing} />
-            <Route component={Routes} />
-          </Switch>
-        </Fragment>
-      </Router>
-    </Provider>
+    <div>
+      <Helmet
+        title='GitTinder'
+        meta={[
+          {
+            name: "description",
+            content:
+              "GitTinder is a social networking and online developer meeting application/website that allows users to connect with similar minds",
+          },
+          {
+            property: "og:description",
+            content:
+              "GitTinder is a social networking and online developer meeting application/website that allows users to connect with similar minds",
+          },
+          {
+            property: "og:title",
+            content: "GitTinder GitHub Tinder DevConnector",
+          },
+          {
+            property: "og:image",
+            content: "http://example.com/article.jpg",
+          },
+          { property: "og:url", content: "http://gittinder.herokuapp.com" },
+          { property: "og:type", content: "social-media" },
+          {
+            name: "keywords",
+            content:
+              "GitHub Tinder GitTinder social-media hub developers connect",
+          },
+          {
+            name: "author",
+            content: "Sarthak Agrawal Significant Hobbies GitTinder",
+          },
+          {
+            name: "google-site-verification",
+            content: "pyKM-6DhxSOA_-VpK-7RIaCk4W8OAi3w5MBzLm05lwA",
+          },
+        ]}
+      />
+      <Provider store={store}>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={Landing} />
+              <Route component={Routes} />
+            </Switch>
+          </Fragment>
+        </Router>
+      </Provider>
+    </div>
   );
 };
 
