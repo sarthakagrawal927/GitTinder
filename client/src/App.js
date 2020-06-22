cb37ef2b82af10ca4a710eed5dffeaf7a4b97e17
Sarthak Agrawal <43884471+sarthakagrawal927@users.noreply.github.com>
2020-06-22 17:23:32 +0530

    react SEO

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import { loadUser } from "./actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 
 import "./App.scss";
+import { Helmet } from "react-helmet";
 
 const App = () => {
   useEffect(() => {
@@ -22,6 +23,41 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
+          <Helmet
+            title='GitTinder'
+            meta={[
+              {
+                name: "description",
+                content:
+                  "GitTinder is a social networking and online developer meeting application/website that allows users to connect with similar minds",
+              },
+              {
+                property: "og:description",
+                content:
+                  "GitTinder is a social networking and online developer meeting application/website that allows users to connect with similar minds",
+              },
+              {
+                property: "og:title",
+                content: "GitTinder GitHub Tinder DevConnector",
+              },
+              {
+                property: "og:image",
+                content: "http://example.com/article.jpg",
+              },
+              { property: "og:url", content: "http://gittinder.herokuapp.com" },
+              { property: "og:type", content: "social-media" },
+              {
+                name: "keywords",
+                content:
+                  "GitHub Tinder GitTinder social-media hub developers connect",
+              },
+              {
+                name: "author",
+                content: "Sarthak Agrawal Significant Hobbies GitTinder",
+              },
+            ]}
+          />
+
           <Navbar />
           <Switch>
             <Route exact path='/' component={Landing} />
